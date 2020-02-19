56a8b3d299492061632581a288ea03b5da68d9af
Branislav Trstenský <bt7s7k7@hotmail.sk>
2020-02-19 21:16:20 +0100

    Fixed isLetter

--- parser.ts
+++ parser.ts
@@ -1,6 +1,6 @@
 export function isLetter(char: string) {
-    return (char.charCodeAt(0) >= "a".charCodeAt(0) && char.charCodeAt(0) >= "z".charCodeAt(0))
-        || (char.charCodeAt(0) >= "A".charCodeAt(0) && char.charCodeAt(0) >= "Z".charCodeAt(0));
+    return (char.charCodeAt(0) >= "a".charCodeAt(0) && char.charCodeAt(0) <= "z".charCodeAt(0))
+        || (char.charCodeAt(0) >= "A".charCodeAt(0) && char.charCodeAt(0) <= "Z".charCodeAt(0));
 }
 
 export interface ICode {
