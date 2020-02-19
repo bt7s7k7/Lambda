261fb37b1722e70a9293f44c65dad0d0afeffc0f
Branislav Trstenský <bt7s7k7@hotmail.sk>
2020-02-19 22:40:54 +0100

    Made error reporting better

--- parser.ts
+++ parser.ts
@@ -6,6 +6,7 @@ export function isLetter(char: string) {
 export interface ICode {
     rootToken: IToken
     text: string
+    error: { location: number, message: string } | null
 }
 
 export enum TokenType {
@@ -65,14 +66,20 @@ export function parseCode(codeText: string) {
                         parent.body = newToken
                         stack.push(newToken)
                     } else {
-                        throw new Error("For some unknown reason an abstraction has a body and argument but is still on the top of the stack")
+                        return makeError("For some unknown reason an abstraction has a body and argument but is still on the top of the stack")
                     }
                 } else {
-                    throw new Error("Wrong parent type")
+                    return makeError("Wrong parent type")
                 }
             } else stack.push(newToken)
 
         }
+        return false
+    }
+
+    var makeError = (message: string) => {
+        code.error = { message, location: pos }
+        return code
     }
 
     for (; pos < codeText.length; pos++) {
@@ -82,7 +89,10 @@ export function parseCode(codeText: string) {
             curr += char
             if (currStart == -1) currStart = pos
         } else {
-            finishIdent()
+            {
+                let ret = finishIdent()
+                if (ret != false) return ret
+            }
             if (char == "(") {
                 let newToken = { start: pos, end: pos + 1, argument: null, type: TokenType.Grouping, code, body: null } as IToken
 
@@ -100,14 +110,14 @@ export function parseCode(codeText: string) {
                         groupStack.push(stack.length)
                     } else if (parent.type == TokenType.Abstraction) {
                         if (parent.argument == null) {
-                            throw new Error("Can't use grouping in function argument")
+                            return makeError("Can't use grouping in function argument")
                         } else {
                             parent.body = newToken
                             stack.push(newToken)
                             groupStack.push(stack.length)
                         }
                     } else {
-                        throw new Error("Wrong parent type")
+                        return makeError("Wrong parent type")
                     }
                 } else {
                     stack.push(newToken)
@@ -116,14 +126,14 @@ export function parseCode(codeText: string) {
 
                 stack.push(newToken)
             } else if (char == ")") {
-                if (groupStack.length == 0) throw Error("Unbalanced bracket at " + pos)
+                if (groupStack.length == 0) return makeError("Unbalanced bracket")
                 stack.length = groupStack[groupStack.length - 1]
-
+                groupStack.pop()
             } else if (char == " ") {
                 // Ignore spaces
             } else if (char == ">") {
                 let newToken = { start: pos, end: pos + 1, argument: null, type: TokenType.Abstraction, code, body: null } as IToken
-                
+
                 if (stack.length != 0) {
                     let parent = stack[stack.length - 1]
                     if (parent.type == TokenType.Identifier || parent.type == TokenType.Application) {
@@ -136,33 +146,36 @@ export function parseCode(codeText: string) {
                         stack.push(newToken)
                     } else if (parent.type == TokenType.Abstraction) {
                         if (parent.argument == null) {
-                            throw new Error("Can't use abstraction in function argument")
+                            return makeError("Can't use abstraction in function argument")
                         } else {
                             parent.body = newToken
                             stack.push(newToken)
                         }
                     } else {
-                        throw new Error("Wrong parent type")
+                        return makeError("Wrong parent type")
                     }
                 } else {
                     stack.push(newToken)
                 }
-                
+
                 stack.push(newToken)
-            } else if (char = "=") {
-                if (stack.length != 1) throw new Error("Binding must be used after one identifier")
+            } else if (char == "=") {
+                if (stack.length != 1) return makeError("Binding must be used after one identifier")
                 let newToken = { start: pos, end: pos + 1, argument: null, type: TokenType.Abstraction, code, body: null } as IToken
                 let ident = stack[0]
                 if (ident.type == TokenType.Identifier) {
                     stack[0] = newToken
                     newToken.argument = ident
-                } else throw new Error("Binding must be used after one identifier")
-                
-            } else throw new Error("Invalid char at " + pos)
+                } else return makeError("Binding must be used after one identifier")
+
+            } else return makeError("Invalid char")
         }
     }
 
-    finishIdent()
+    {
+        let ret = finishIdent()
+        if (ret != false) return ret
+    }
 
     code.rootToken = stack[0]
 
@@ -182,13 +195,17 @@ export function debugCode(code: ICode) {
     var ret = []
     ret.push(code.text)
 
-    var visit = (token: IToken, path: string) => {
-        ret.push(" ".repeat(token.start) + "-".repeat(token.end - token.start) + " ".repeat(code.text.length - token.end + 1) + path)
-        if (token.argument) visit(token.argument, path + "/a")
-        if (token.body) visit(token.body, path + "/b")
-    }
+    if (code.error) {
+        ret.push(" ".repeat(code.error.location) + "^ " + code.error.message)
+    } else {
+        var visit = (token: IToken, path: string) => {
+            ret.push(" ".repeat(token.start) + "-".repeat(token.end - token.start) + " ".repeat(code.text.length - token.end + 1) + path)
+            if (token.argument) visit(token.argument, path + "/a")
+            if (token.body) visit(token.body, path + "/b")
+        }
 
-    if (code.rootToken) visit(code.rootToken, "")
+        if (code.rootToken) visit(code.rootToken, "")
+    }
 
     return ret.join("\n")
 }
