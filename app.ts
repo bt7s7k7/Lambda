374bfeb292cb7125c68ad67e6cd891027a9bc26c
Branislav Trstenský <bt7s7k7@hotmail.sk>
2020-02-20 13:25:22 +0100

    Better looking output

--- app.ts
+++ app.ts
@@ -5,17 +5,21 @@ import { inspect } from "util"
 
 var prompt = createInterface({
     input: process.stdin,
-    output: process.stdout
+    output: process.stdout,
+    prompt: ""
 })
 
 var state = new State()
 
 prompt.on("line", (line) => {
     var code = parseCode(line)
+    console.log("")
     console.log(debugCode(code))
+    console.log("")
     if (!isError(code)) {
         let ret = state.evalCode(code)
         if (isError(ret)) console.log(debugCode(ret))
         else console.log(debugValue(ret))
     }
+    console.log("")
 })
