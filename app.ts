00cb1d0bbff96e48d314be40816808db7c2647fd
Branislav Trstenský <bt7s7k7@hotmail.sk>
2020-02-20 14:25:12 +0100

    Made importable modules

--- app.ts
+++ app.ts
@@ -11,25 +11,54 @@ var prompt = createInterface({
 
 var state = new State()
 
-for (let line of `
+
+
+var codeModules = {
+    std: `
 I = >a a
 K = >a>b a
 KI = K I
 C = >f>a>b f b a
-`.split("\n")) {
-    let code = parseCode(line)
-    if (!isError(code)) state.evalCode(code)
-    else throw new Error("Error in bootstraper:\n" + debugCode(code))
+`,
+    logic: `
+true = K
+false = KI
+not = >a a false true
+and = >a>b a (b true false) false
+or = >a>b a true (b true false)
+`
+}
+
+function importCode(name: string) {
+    if (name in codeModules) {
+        for (let line of codeModules[name].split("\n")) {
+            let code = parseCode(line)
+            if (!isError(code)) state.evalCode(code)
+            else throw new Error("Error in module:\n" + debugCode(code))
+        }
+    } else console.log("Unknown module")
 }
 
 var debugLogging = false
+
+importCode("std")
 
 prompt.on("line", (line) => {
     if (line == "debug") {
         debugLogging = !debugLogging
         console.log(`\nDebug mode is now ${debugLogging ? "enabled" : "disabled"}\n`)
         return
-     }
+    }
+    
+    {
+        let split = line.split(" ")
+        if (split[0] == "import") {
+            importCode(split[1])
+            console.log(`Imported ${split[1]}`)
+            return
+        }
+    }
+
     var code = parseCode(line)
     console.log("")
     if (debugLogging || isError(code)) {
